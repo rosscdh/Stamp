205bff68fa2743a7f3d208a71265491902bb7e81
Ross Crawford-d'Heureuse <ross@lawpal.com>
2013-12-02 11:18:42 +0100

    renamed

---
+++ stamp.rb
@@ -0,0 +1,31 @@
+require 'grape'
+require './services'
+
+module Stamp
+  class API < Grape::API
+
+    version 'v1', using: :path
+
+    resource :html do
+
+      desc "Post HTML get PDF in response"
+      params do
+        requires :html, type: String, desc: "Raw HTML"
+        requires :filename, type: String, desc: "Name of file to generate"
+      end
+      post "/to/pdf" do
+        html = params[:html]
+        filename = params[:filename]
+
+        service = Stamp::HTML2PDFService.new html, filename
+
+        content_type "application/octet-stream"
+        header['Content-Disposition'] = "attachment; filename=%s" % filename
+        env['api.format'] = :binary
+
+        body service.as_inline_pdf
+      end
+    end
+
+  end
+end
